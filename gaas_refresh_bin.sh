1a97e601de1ce18326142998ad82002bbea3cd41
Jacques Dainat <jacques.dainat@sys2diag.cnrs.fr>
2022-12-02 14:29:04 +0100

    update script to refresh bin prompting to user for new script + populate bin from original exe files

--- gaas_refresh_bin.sh
+++ gaas_refresh_bin.sh
@@ -9,25 +9,36 @@ if [[ ! ${PWD##*/} == "bin" ]];then
 fi
 
 
-#remove scripts
-for j in *;do
-        name=$(basename $j)
-        unlink $name
-done
-
 #While looking at all script within the repo we skip all Deprecated folder and what it contains
-for i in $(find ../ -not \( -path */Deprecated -prune \) -not \( -path */bin -prune \)  -not \( -path */blib -prune \) -name '*.pl' -o -name '*.sh' -o -name '*.py' -o -name '*.r' -o -name '*.R' -o -name '*.rb');do
+for i in $(find ../ -not \( -path */Deprecated -prune \) -not \( -path */terraform/* -prune \) -not \( -path */bin -prune \)  -not \( -path */blib -prune \) -name '*.pl' -o -name '*.sh' -o -name '*.py' -o -name '*.r' -o -name '*.R' -o -name '*.rb');do
 
         name=$(basename $i)
 
 	# skip gaas_refresh_list.sh because must not be in the bin to avoid to be distributed
-	if [ $name == "gaas_refresh_bin.sh"  ] ; then
+	if [[ $name == "gaas_refresh_bin.sh" || $name == "run_proxy.sh" || $name == "refresh_cert.sh" 
+		|| $name == "get_cert_first.sh" || $name == "verify.sh"  ]] ; then
 		continue
 	fi
 
-        #Populate scripts using hard link
+        # add new script
         if [[ ! -f ${name} ]];then
-                ln $i ${name}
-        fi
 
+	    while true; do
+	    	copyfile="no"
+	    	read -p "The script ${name} does not exist in /bin. Do you want to add it? (yes/y or no/n)" yn
+	    	case $yn in
+	        	[Yy]* ) copyfile="yes"; break;;
+	        	[Nn]* ) break;;
+	        	* ) echo "Please answer yes or no.";;
+	    	esac
+	    done
+
+	    if [[ $copyfile == "yes" ]];then
+	    	cp $i .
+	    fi
+
+        else # upadte script
+            echo -e "copy $i into /bin"
+            cp $i .
+        fi
 done
